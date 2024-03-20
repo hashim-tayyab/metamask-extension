83cfa9cc49c89799b43a7c90a73add157e05ced4
Sylva Elendu <iamsylvaelendu@gmail.com>
2024-03-20 11:17:29 +0100

    lint

--- ui/helpers/utils/settings-search.test.js
+++ ui/helpers/utils/settings-search.test.js
@@ -162,7 +162,7 @@ describe('Settings Search Utils', () => {
     it('returns "Security & privacy" section count', () => {
       expect(
         getNumberOfSettingRoutesInTab(t, t('securityAndPrivacy')),
-      ).toStrictEqual(18);
+      ).toStrictEqual(17);
     });
 
     it('returns "Alerts" section count', () => {
